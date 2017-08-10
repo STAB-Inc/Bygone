d355fb8fa2b03e726077791d0b8e3338ca08034d
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-08-10 19:50:51 +1000

    Added scripts for index

---
+++ public/javascripts/main_script.js
@@ -0,0 +1,53 @@
+$(document).ready( function () {
+  'use strict';
+
+  var toggleState;
+  $('#index-nav .index').click( function (e) {
+    e.preventDefault();
+    if ($(this).hasClass('no-redirect')) {
+      initButtonToggle(this);
+    }
+    else {
+      console.log('redirect')
+    }
+  });
+
+  function initButtonToggle(_this) {
+    if (!toggleState) {
+      $(_this).find('.bg').css({
+        'width': '100%',
+        'left': '0' 
+      });
+      $(_this).find('h5').css({
+        'color': 'white',
+      });
+      $(_this).find('.chapter-selection').css({
+        'margin-left': '0'
+      });
+      $(_this).find('.chapter-selection a').css({
+        'transform': 'rotate(0)',
+        'opacity': 1,
+        'margin': '0 10px 0 10px'
+      });
+    }
+    else {
+      $(_this).find('.bg').css({
+        'width': '',
+        'left': '' 
+      });
+      $(_this).find('h5').css({
+        'color': '',
+      });
+      $(_this).find('.chapter-selection').css({
+        'margin-left': ''
+      });
+      $(_this).find('.chapter-selection a').css({
+        'transform': '',
+        'opacity': '',
+        'margin': ''
+      });
+    }
+    toggleState = !toggleState;
+  }
+
+});
