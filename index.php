9b870fc72dced94ab0ef40dc8695fb6a6c609ae7
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-09-09 02:05:26 +1000

    Rebuild in index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include "views/layout.html" ?>
+  <body id="index">
+    <img src="index.jpg" />
+    <main>
+      <div class="container-fluid">
+        <div class="row header">
+          <h1>BYGONE</h1>
+          <h3> A Soldier's Tale</h3>
+        </div>
+        <div class="row">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="row buttons">
+          <div class="row">
+            <div class="col-lg-7">
+              <div class="buttonContainer">
+                <h3><b>PLAY</b></h3>
+              </div>
+            </div>
+            <div class="col-lg-offset-1 col-lg-4">
+              <div class="buttonContainer">
+                <h3>HIGH SCORES</h3>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-7">
+              <div class="buttonContainer">
+                <h3><b>FREE MODE</b></h3>
+              </div>
+            </div>
+            <div class="col-lg-offset-1 col-lg-4">
+              <div class="buttonContainer">
+                <h3>ABOUT</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </main>
+    <section id="campaign">
+
+    </section>
+  </body>
+</html>
