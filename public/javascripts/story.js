ed56c43803cbfa43d110691e14e72610fd9d8e41
shawn10969 <1064527348@qq.com>
2017-09-13 20:01:35 +1000

    Adjustmented background color & text location

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -3,6 +3,8 @@ var step = 0;
 $(document).ready(function(){
   $(".loaderContent h1").css("font-weight", "900");
 
+  $("#story").css("background-color", "#101010");
+
   $("#chap1sub p").hide();
   $("#chap1img p").hide();
   $("#house").hide();
