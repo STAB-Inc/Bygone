68d8cb9d5448d2a2a0d05976004103a579794719
shawn10969 <1064527348@qq.com>
2017-09-17 20:14:16 +1000

    Added chapter4

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
     $("#house").delay(2000).fadeIn(2000,"linear");
     $("#recruitOffice").delay(2000).fadeIn(2000,"linear");
     $("#watchtowerOut").delay(2000).fadeIn(2000,"linear");
+    $("#warEnds").delay(2000).fadeIn(2000,"linear");
   });
 
   $("#house").click(function(){
@@ -149,6 +150,64 @@ $(document).ready(function(){
       $("#mattAiming").delay(1500).fadeIn(1000,"linear");
       $("#chap3sub p:nth-of-type("+ 3 +")").delay(1500).fadeIn(1000,"linear");
     }else if(step == 4){
+      $("#back").fadeIn(500,"linear");
+    }
+  });
+
+  $("#warEnds").click(function(){
+    $("#warEnds").fadeOut(1000,"linear");
+    $("#mattHome").delay(1500).fadeIn(1000,"linear");
+    $("#chap4sub1 p:nth-of-type("+ 1 +")").delay(1500).fadeIn(1000,"linear");
+  });
+
+  $("#chap4img1").click(function(){
+    step = step + 1;
+    if(step == 1){
+      $("#chap4sub1 p:nth-of-type("+ 1 +")").fadeOut(500,"linear");
+      $("#mattHome").fadeOut(1000,"linear");
+      $("#pourMilk").delay(1000).fadeIn(1000,"linear");
+      $("#chap4sub1 p:nth-of-type("+ 2 +")").delay(1000).fadeIn(1000,"linear");
+    }else if(step == 2){
+      $("#chap4sub1 p:nth-of-type("+ 2 +")").fadeOut(500,"linear");
+      $("#pourMilk").delay(1000).fadeOut(100,"linear");
+      $("#flashBackBlur").delay(500).fadeIn(100,"linear");
+      $("#flashBack").delay(600).fadeIn(300,"linear");
+    }else if(step == 3){
+      $("#flashBack").fadeOut(300,"linear");
+      $("#flashBackBlur").delay(100).fadeOut(1000,"linear");
+      $("#dropMilk1").delay(1100).fadeIn(1000,"linear");
+      $("#dropMilk2").delay(2100).fadeIn(1000,"linear");
+    }else if(step == 4){
+      $("#dropMilk1").fadeOut(0);
+      $("#dropMilk2").fadeOut(1000,"linear");
+      $("#roomBlur").delay(1000).fadeIn(1000,"linear");
+      $("#mattDrink").delay(1000).fadeIn(1000,"linear");
+      $("#chap4sub1 p:nth-of-type("+ 3 +")").delay(1000).fadeIn(1000,"linear")
+    }else if(step == 5){
+      $("#roomBlur").fadeOut(1500,"linear");
+      $("#mattDrink").fadeOut(1500,"linear");
+      $("#chap4sub1 p:nth-of-type("+ 3 +")").fadeOut(1000,"linear");
+      $("#office").delay(1500).fadeIn(1000,"linear");
+      $("#mattSuit").delay(1500).fadeIn(1000,"linear");
+      $("#boss").delay(1500).fadeIn(1000,"linear");
+      step = 0
+    }
+  });
+
+  $("#chap4img2").click(function(){
+    if(step < 4){
+      $("#chap4sub2 p:nth-of-type("+ step +")").fadeOut(500,"linear");
+      step = step + 1;
+      $("#chap4sub2 p:nth-of-type("+ step +")").delay(500).fadeIn(1000,"linear");
+    }else if(step == 4){
+      $("#chap4sub2 p:nth-of-type("+ step +")").fadeOut(500,"linear");
+      step = step + 1;
+      $("#office").fadeOut(1000,"linear");
+      $("#boss").fadeOut(500,"linear");
+      $("#mattSuit").fadeOut(500,"linear");
+      $("#mattHand").delay(1000).fadeIn(1500,"linear");
+      $("#chap4sub2 p:nth-of-type("+ step +")").delay(1500).fadeIn(1000,"linear");
+    }else if (step == 5){
       $("#back").fadeIn(500,"linear");
     }
   });
