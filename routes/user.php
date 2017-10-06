67dcd0e543c853f3c75620367421a38f4198d528
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-10-06 16:24:56 +1000

    Method for new json users

--- routes/user.php
+++ routes/user.php
@@ -3,12 +3,20 @@
   if (!file_exists($file)) {
     fopen($file, 'w') or die('err');
   };
+
   if(isset($_REQUEST['new']))
   {
     echo $_POST['username'];
+    $users = getUsers($file);
     $userData['id'] = uniqid();
     $userData['username'] = $_POST['username'];
     $userData['password'] = $_POST['password'];
-    file_put_contents($file, json_encode($userData));
-  }
+    array_push($users, $userData);
+    file_put_contents($file, json_encode($users));
+  };
+
+  function getUsers($file) {
+    return json_decode(file_get_contents($file), true);
+  };
+
 ?>
