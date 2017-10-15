8e960188f4eeb02679b169b8bde9878d6684f8c6
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-10-15 18:04:26 +1000

    Show current user on highscores

--- public/javascripts/highscores.coffee
+++ public/javascripts/highscores.coffee
@@ -13,26 +13,44 @@ $(document).ready ->
     method: 'getAll'
   }).then (res) ->
     userData = JSON.parse res
-    gameScore = {
-      g1: []
-      g2: []
-      g3: []
-      g4: []
-    }
-    for key in Object.keys gameScore
-      for data in userData
-        for scores in data.scores[key]
-          gameScore[key].push [parseInt(scores[0]), scores[1], data.username]
-      for score in gameScore[key].sort sortDec
-        $('.' + key).append $('<div class="row">
-                                <div class="col-lg-4">
-                                <p>' + score[2] + '</p>
-                                </div>
-                                <div class="col-lg-4">
-                                  <p>' + score[0] + '</p>
-                                </div>
-                                <div class="col-lg-4">
-                                  <p class="date">' + score[1] + '</p>
-                                </div>
-                              </div>')
-    console.log gameScore
+    submitUserData({
+      method: 'getActiveUser'
+    }).then (user) ->
+      user = JSON.parse user
+      userId = user.message
+      gameScore = {
+        g1: []
+        g2: []
+        g3: []
+        g4: []
+      }
+      for key in Object.keys gameScore
+        for data in userData
+          for scores in data.scores[key]
+            gameScore[key].push [parseInt(scores[0]), scores[1], data.username, data.id]
+        for score in gameScore[key].sort sortDec
+          if userId == score[3]
+            $('.' + key).append $('<div class="row current">
+                                    <div class="col-lg-4">
+                                    <p>' + score[2] + '</p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                      <p>' + score[0] + '</p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                      <p class="date">' + score[1] + '</p>
+                                    </div>
+                                  </div>')
+          else
+            $('.' + key).append $('<div class="row">
+                          <div class="col-lg-4">
+                          <p>' + score[2] + '</p>
+                          </div>
+                          <div class="col-lg-4">
+                            <p>' + score[0] + '</p>
+                          </div>
+                          <div class="col-lg-4">
+                            <p class="date">' + score[1] + '</p>
+                          </div>
+                        </div>')
+    #console.log gameScore
--- public/javascripts/highscores.js
+++ public/javascripts/highscores.js
@@ -22,32 +22,49 @@
     return submitUserData({
       method: 'getAll'
     }).then(function(res) {
-      var data, gameScore, i, j, k, key, l, len, len1, len2, len3, ref, ref1, ref2, score, scores, userData;
+      var userData;
       userData = JSON.parse(res);
-      gameScore = {
-        g1: [],
-        g2: [],
-        g3: [],
-        g4: []
-      };
-      ref = Object.keys(gameScore);
-      for (i = 0, len = ref.length; i < len; i++) {
-        key = ref[i];
-        for (j = 0, len1 = userData.length; j < len1; j++) {
-          data = userData[j];
-          ref1 = data.scores[key];
-          for (k = 0, len2 = ref1.length; k < len2; k++) {
-            scores = ref1[k];
-            gameScore[key].push([parseInt(scores[0]), scores[1], data.username]);
+      return submitUserData({
+        method: 'getActiveUser'
+      }).then(function(user) {
+        var data, gameScore, i, j, k, key, len, len1, len2, ref, ref1, results, score, scores, userId;
+        user = JSON.parse(user);
+        userId = user.message;
+        gameScore = {
+          g1: [],
+          g2: [],
+          g3: [],
+          g4: []
+        };
+        ref = Object.keys(gameScore);
+        results = [];
+        for (i = 0, len = ref.length; i < len; i++) {
+          key = ref[i];
+          for (j = 0, len1 = userData.length; j < len1; j++) {
+            data = userData[j];
+            ref1 = data.scores[key];
+            for (k = 0, len2 = ref1.length; k < len2; k++) {
+              scores = ref1[k];
+              gameScore[key].push([parseInt(scores[0]), scores[1], data.username, data.id]);
+            }
           }
+          results.push((function() {
+            var l, len3, ref2, results1;
+            ref2 = gameScore[key].sort(sortDec);
+            results1 = [];
+            for (l = 0, len3 = ref2.length; l < len3; l++) {
+              score = ref2[l];
+              if (userId === score[3]) {
+                results1.push($('.' + key).append($('<div class="row current"> <div class="col-lg-4"> <p>' + score[2] + '</p> </div> <div class="col-lg-4"> <p>' + score[0] + '</p> </div> <div class="col-lg-4"> <p class="date">' + score[1] + '</p> </div> </div>')));
+              } else {
+                results1.push($('.' + key).append($('<div class="row"> <div class="col-lg-4"> <p>' + score[2] + '</p> </div> <div class="col-lg-4"> <p>' + score[0] + '</p> </div> <div class="col-lg-4"> <p class="date">' + score[1] + '</p> </div> </div>')));
+              }
+            }
+            return results1;
+          })());
         }
-        ref2 = gameScore[key].sort(sortDec);
-        for (l = 0, len3 = ref2.length; l < len3; l++) {
-          score = ref2[l];
-          $('.' + key).append($('<div class="row"> <div class="col-lg-4"> <p>' + score[2] + '</p> </div> <div class="col-lg-4"> <p>' + score[0] + '</p> </div> <div class="col-lg-4"> <p class="date">' + score[1] + '</p> </div> </div>'));
-        }
-      }
-      return console.log(gameScore);
+        return results;
+      });
     });
   });
 
